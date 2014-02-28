e1a83b30ba31cae72dff7ee712b61c12f0e80d87
Apprentice <apprentice@DBC-Infinity-2.local>
2014-02-27 20:25:20 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waste5::Application.config.secret_token = '6cb6051f284f28de79da1f8bb524cf1d33d7b1943474b49ad49745d9639ce7e467f3cddae85f32daeec52539d5addb0241365bb42989d7ac463e256bb840b0fd'
